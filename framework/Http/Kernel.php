e60957d5e5b83ee476981306b02c5ef4d250409e
Alexey Samoylik <samoilikaleksey@yandex.ru>
2024-07-27 23:38:59 +0300

    разработка роутинга

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,12 +2,33 @@
 
 namespace Alexeysmlk\Framework\Http;
 
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
-    public function handle(Request $request): Response
-    {
-        $content = '<h1>Hello world</h1>';
+	public function handle(Request $request): Response
+	{
+		$dispatcher = simpleDispatcher(function (RouteCollector $collector) {
+			$collector->get( '/', function () {
+				$content = '<h1>Hello world22</h1>';
 
-        return new Response($content);
-    }
+				return new Response($content);
+			});
+			$collector->get('/posts/{id:\d+}', function (array $vars) {
+				$content = "<h1>Post - {$vars['id']}</h1>";
+
+				return new Response($content);
+			});
+		});
+
+		$routeInfo = $dispatcher->dispatch(
+			$request->getMethod(),
+			$request->getPath(),
+		);
+
+		[$status, $handler, $vars] = $routeInfo;
+
+		return $handler($vars);
+	}
 }
