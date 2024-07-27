98d1fc3d294b2ff0329941a4de59cf1fce55e9ac
Alexey Samoylik <samoilikaleksey@yandex.ru>
2024-07-27 23:39:49 +0300

    форматирование

--- framework/Http/Response.php
+++ framework/Http/Response.php
@@ -4,16 +4,16 @@ namespace Alexeysmlk\Framework\Http;
 
 class Response
 {
-    public function __construct(
-        private mixed $content,
-        private int $statusCode = 200,
-        private array $headers = [],
-    )
-    {
-    }
+	public function __construct(
+		private mixed $content,
+		private int $statusCode = 200,
+		private array $headers = [],
+	)
+	{
+	}
 
-    public function send(): void
-    {
-        echo $this->content;
-    }
+	public function send(): void
+	{
+		echo $this->content;
+	}
 }
