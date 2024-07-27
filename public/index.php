ca91e9039a677a6615de94239d30fe9c1310299d
Alexey Samoylik <samoilikaleksey@yandex.ru>
2024-07-27 18:06:02 +0300

    создание класса response и его применение

--- public/index.php
+++ public/index.php
@@ -3,5 +3,11 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Alexeysmlk\Framework\Http\Request;
+use Alexeysmlk\Framework\Http\Response;
 
 $request = Request::createFromGlobals();
+
+$content = '<h1>Hello world</h1>';
+
+$response = new Response($content);
+$response->send();
