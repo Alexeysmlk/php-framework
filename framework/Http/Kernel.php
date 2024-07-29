a44b45318c06ebf103d9770489ff2a2d891b2f62
Alexey Samoylik <samoilikaleksey@yandex.ru>
2024-07-29 21:01:49 +0300

    упрощение обработки ошибок

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace Alexeysmlk\Framework\Http;
 
+use Alexeysmlk\Framework\Http\Exceptions\HttpException;
+use Alexeysmlk\Framework\Http\Exceptions\MethodNotAllowedException;
+use Alexeysmlk\Framework\Http\Exceptions\RouteNotFoundException;
 use Alexeysmlk\Framework\Routing\RouterInterface;
 
 class Kernel
@@ -17,8 +20,10 @@ class Kernel
 			[$routeHandler, $vars] = $this->router->dispatch($request);
 
 			$response = call_user_func_array($routeHandler, $vars);
-		} catch (\Throwable $exception) {
-			$response = new Response($exception->getMessage(), 500);
+		} catch (HttpException $e) {
+			$response = new Response($e->getMessage(), $e->getStatusCode());
+		} catch (\Throwable $e) {
+			$response = new Response($e->getMessage(), 500);
 		}
 
 		return $response;
